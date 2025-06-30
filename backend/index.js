ea25ecf3aae8a797624c4540a5a564cee48f83ec
zainal <133510409+pongnal@users.noreply.github.com>
2025-06-30 21:07:47 +0800

    backend
    - install  bcrypt
    - install mongose
    
    - create endpoint for (register, login, getAll user)
    - use mongo compass

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
- const app = express();
- 
- app.get("/", (req, res) => {
-   res.send("Welcome to my backend!");
- });
- 
- app.listen(3000, () => {
-   console.log("Server is running on http://localhost:3000");
- });
+const mongoose = require("mongoose");
+const cors = require("cors")
+
+const authRoutes = require("./routes/auth");
+
+mongoose.connect("mongodb://localhost:27017/")
+    .then(() => {
+        const app = express();
+        app.use(cors({
+            origin: '*', // allow all origins
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            credentials: true,
+        }));
+        app.use(express.json());
+
+        app.use("/auth", authRoutes);
+
+        app.listen(3000, () => console.log("Task Manager API running on port 3000"));
+});
---
+++ backend/routes/auth.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const router = express.Router();
+
+// Endpoint to register
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+  const user = await User.create({ email: req.body.email, password: hashed });
+  res.status(201).json({ message: "User registered" });
+});
+//no logic that detect if the user is already registered
+
+
+
+// Endpoint to login
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  
+  if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+    return res.status(401).send("Invalid credentials");
+  }
+
+  const token = jwt.sign({ id: user._id }, "w4h9V7xYpL3QmZ8tR2fN6jBvXsC1KdPzF0qW8eYtUaMvJrXn", { expiresIn: "1h" });
+  res.json({ token });
+});
+
+
+// Endpoint to get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({}, { email: 1, password: 1, _id: 0 });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
+
+// Endpoint to test the server (auth)
+router.post("/test", async (req, res) => {
+  console.log("Test")
+  res.send("Testing ok!")
+})
+
+module.exports = router;
